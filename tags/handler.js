1a1d54694aa11c39c8bcea4a5828e4881a2a7453
jayraymou <63583542+jayraymou@users.noreply.github.com>
2020-10-19 00:47:17 -0400

    Update handler.js

--- tags/handler.js
+++ tags/handler.js
@@ -45,7 +45,7 @@ function request(source, site, regex) {
                                             break;
                                             default:
                                                 // if the media thumbnail is from gfycat try again (thumbnails from gfycat are really low res)
-                                                switch (post.media.oembed.thumbnail_url.includes("gfcat")) {
+                                                switch (post.media.oembed.thumbnail_url === ("gfycat")) {
                                                     case false:
                                                         // resolve payload
                                                         let payload = {
